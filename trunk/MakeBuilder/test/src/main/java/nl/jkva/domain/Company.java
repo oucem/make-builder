63e8b7dead9d305fc5bab429a54c3d3fc8ce40d2
jankeesvanandel <jankeesvanandel@6c687b58-11d3-11df-992a-f3fa5211fe9f>
2010-02-09 18:46:37 +0000

    Initial commit

---
+++ trunk/MakeBuilder/test/src/main/java/nl/jkva/domain/Company.java
@@ -0,0 +1,32 @@
+//package nl.jkva.domain;
+//
+//import nl.jkva.builder.Immutable;
+//import nl.jkva.builder.ImmutableObject;
+//import nl.jkva.builder.ObjectBuilder;
+//
+//import java.util.Date;
+//import java.util.Set;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: Jan-Kees
+// * Date: 1-nov-2009
+// * Time: 15:04:41
+// * To change this template use File | Settings | File Templates.
+// */
+//@Immutable
+//public interface Company extends ImmutableObject<Company> {
+//    long getId();
+//
+//    String getName();
+//
+//    Address getAddress();
+//
+//    Address getPostalAddress();
+//
+//    Date getDateFounded();
+//
+//    Set<Person> getEmployees();
+//
+//    ObjectBuilder<Company> builder();
+//}
